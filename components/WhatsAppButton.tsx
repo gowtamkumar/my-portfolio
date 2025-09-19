55ef451b2fb3c8a943de73f4f7b6d11605cad353
gowtamkumar <gowtampaul0@gmail.com>
2025-09-19 11:38:36 +0600

    whatsapp update

--- components/WhatsAppButton.tsx
+++ components/WhatsAppButton.tsx
@@ -17,7 +17,7 @@ const WhatsAppButton: React.FC<WhatsAppButtonProps> = ({
       href={url}
       target="_blank"
       rel="noopener noreferrer"
-      className="fixed bottom-5 right-5 bg-green-500 text-white p-4 rounded-full shadow-lg hover:bg-green-600 transition"
+      className="fixed bottom-5 right-5 z-10 bg-green-500 text-white p-4 rounded-full shadow-lg hover:bg-green-600 transition"
     >
       <FaWhatsapp size={24} />
     </a>
